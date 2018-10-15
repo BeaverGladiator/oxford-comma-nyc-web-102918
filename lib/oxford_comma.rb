3c5f3edc07825c87252f4c2126187fed3ed7a265
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 15:40:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.count == 2 
     array.join(" and ")
   elsif array.count > 2 
-   array[-1] 
+   last_item = array.pop
    array.join(", ")
   else 
     array.join
