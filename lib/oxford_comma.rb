e1868bfa308dc651939e799f7870e60e8cca4935
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 15:39:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.count == 2 
     array.join(" and ")
   elsif array.count > 2 
-   array.insert(-1, "and #{array[-1]}")
-  array.join(", ")
+   array[-1] 
+   array.join(", ")
   else 
     array.join
   end
