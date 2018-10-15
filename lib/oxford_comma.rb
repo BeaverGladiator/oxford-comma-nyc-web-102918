3c957d2c046f47f6d08f8933880a6837e15b2e75
Matthew Migliore <mmigliore311@gmail.com>
2018-10-15 15:41:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   if array.count == 2 
     array.join(" and ")
   elsif array.count > 2 
-   last_item = array.pop
+   last_item = array.pop()
+   array << "and #{last_item}"
    array.join(", ")
   else 
     array.join
